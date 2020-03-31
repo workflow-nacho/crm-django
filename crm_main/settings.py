65d2b7303c6f26b26628d0a2df2810da3e6b441b
Nacho <joseignaciorm13@gmail.com>
2020-03-31 13:41:04 +0200

    Create tmplates, apps,media and static folders, added template path, static root and url, media root and url, gather all static file of project to static folder

--- crm_main/settings.py
+++ crm_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'crm_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
